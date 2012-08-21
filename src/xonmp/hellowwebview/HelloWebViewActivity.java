c5b3d613d0c275f05aff319fa2a156aaa5120c06
TheNotary <no@mail.plz>
2012-08-20 19:47:03 -0500

    having lots of trouble putting together the finishing touches on blocking off the browser's double tap feature.

--- src/xonmp/hellowwebview/HelloWebViewActivity.java
+++ src/xonmp/hellowwebview/HelloWebViewActivity.java
@@ -1,12 +1,9 @@
 package xonmp.hellowwebview;
 
 import android.app.Activity;
-import android.content.Context;
-import android.content.res.Configuration;
 import android.os.Bundle;
-import android.view.GestureDetector;
+import android.util.Log;
 import android.view.KeyEvent;
-import android.view.MotionEvent;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -15,31 +12,30 @@ import xonmp.hellowwebview.MyWebView;
 
 
 public class HelloWebViewActivity extends Activity {
-	private WebView mWebView;
-	//private MyWebView mWebView;
+	//private WebView mWebView;
+	private MyWebView mWebView;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
-        mWebView = (WebView) findViewById(R.id.webview);
-        //mWebView = (MyWebView) findViewById(R.id.webview);
+        Log.d("We Got Here", "By here I mean onCreate");
+        
+        //mWebView = (WebView) findViewById(R.id.webview);
+        mWebView = (MyWebView) findViewById(R.id.webview);
+        
         
         WebSettings config = mWebView.getSettings();
         config.setJavaScriptEnabled(true);
         config.setLoadWithOverviewMode(true);
         config.setUseWideViewPort(true);
         config.setBuiltInZoomControls(false);
-        //config.setSupportZoom(false); 
-        // config.setUseWideViewPort(false);  // disables zooming altogether...
-
-
+        config.setSupportZoom(false); 
         // config.setUserAgentString("android-client"); 
+        
         mWebView.loadUrl("http://192.168.0.11:3000/game.html");
-        mWebView.canScrollHorizontally(0);
         mWebView.setWebViewClient(new HelloWebViewClient());
-        //boolean isIt = mWebView.isHardwareAccelerated();
     }
     
     private class HelloWebViewClient extends WebViewClient{
@@ -59,11 +55,6 @@ public class HelloWebViewActivity extends Activity {
     	}
     	return super.onKeyDown(keyCode,  event);
     }
-    
-    @Override
-    public void onConfigurationChanged(Configuration newConfig){
-        super.onConfigurationChanged(newConfig);
-    } 
 }
 
 
--- src/xonmp/hellowwebview/MyWebView.java
+++ src/xonmp/hellowwebview/MyWebView.java
@@ -1,35 +1,62 @@
 package xonmp.hellowwebview;
 
 import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.webkit.WebView;
+import android.widget.Toast;
 
 
 class MyWebView extends WebView { 
+	boolean doubleTap = false;
 	
     public MyWebView(Context context) {
 		super(context);
 	}
+    
+    public MyWebView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
 
-/*
+    @Override
 	public boolean onTouchEvent(MotionEvent event) {
-		GestureDetector gd = new GestureDetector(sogl);
-		gd.onTouchEvent(event);
-        // disable double tap zooming
-		boolean doubleTap = true;
-        if(doubleTap){
-            doubleTap = false;
-            return false;
-        }
-            return super.onTouchEvent(event);
+		Log.d("113133", "onTouchEvent Fired!");
+		if(true){
+			GestureDetector gd = new GestureDetector(sogl);
+			gd.onTouchEvent(event);
+			
+	        // disable double tap zooming
+	        if(doubleTap){
+	            doubleTap = false;
+	            return false;
+	        }
+	            return super.onTouchEvent(event);
+		}
+		
+		return super.onTouchEvent(event);
     }/**/
     
-/*
-	GestureDetector.SimpleOnGestureListener sogl = new GestureDetector.SimpleOnGestureListener() { 
-	    public boolean onDoubleTap(MotionEvent e) {
-            //showToast("Double tap");
-            //doubleTap = true;
 
+	GestureDetector.SimpleOnGestureListener sogl = new GestureDetector.SimpleOnGestureListener() { 
+		public boolean onDown(MotionEvent e){
+			Log.d("ohmygosh", "ohmygosh - onDown");
+			return false;
+		}
+	    public boolean onDoubleTap(MotionEvent e) {
+            doubleTap = true;
+            Log.d("ohmygosh", "ohmygosh - onDoubleTap");
             return false;
-	    }  
+	    }
 	};/**/
+    
+    /*
+    @Override
+    public boolean onTouchEvent(MotionEvent event){
+    	Log.d("113133", "onTouchEvent Fired!");
+    	//float myF = 9/0;
+    	
+    	return true;
+    }/**/
 }
